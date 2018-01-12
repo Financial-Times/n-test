8e0c3ecbedfbd2b144c5afe920a1acdc948d447a
Arjun Gadhia <arjungadhia@gmail.com>
2018-01-12 16:31:10 +0000

    Make coverage tests pass  🐿 v2.6.0

--- test/server/app.js
+++ test/server/app.js
@@ -6,6 +6,48 @@ app.get('/status/:status', (req, res) => {
 	res.status(req.params.status).send();
 });
 
+app.get('/coverage/good', (req, res) => {
+	res.send(`
+		<body>
+			<style>
+				.one { background: red; }
+				.two { background: red; }
+				.three { background: red; }
+			</style>
+			<div class="one"></div>
+			<div class="two"></div>
+			<div class="three"></div>
+		</body>
+	`);
+});
+
+app.get('/coverage/okay', (req, res) => {
+	res.send(`
+		<body>
+			<style>
+				.one { background: red; }
+				.two { background: red; }
+				.three { background: red; }
+			</style>
+			<div class="one"></div>
+			<div class="two"></div>
+		</body>
+	`);
+});
+
+app.get('/coverage/bad', (req, res) => {
+	res.send(`
+		<body>
+			<style>
+				.one { background: red; }
+				.two { background: red; }
+				.three { background: red; }
+			</style>
+			<div class="one"></div>
+		</body>
+	`);
+});
+
 if (!module.parent) {
 	app.listen(process.env.PORT || 3004);
 } else {
