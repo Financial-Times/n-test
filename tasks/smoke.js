602a39c2c554d47d2a6b3f4005670776ad5cf1a8
Arjun Gadhia <arjungadhia@gmail.com>
2018-01-15 12:05:04 +0000

    Update smoke test help command  🐿 v2.6.0

--- tasks/smoke.js
+++ tasks/smoke.js
@@ -7,7 +7,7 @@ module.exports = (program) => {
 		.command('smoke')
 		// .option('--auth', 'Authenticate with FT_NEXT_BACKEND_KEY')
 		.option('-h, --host [value]', 'Set the hostname to use for all tests')
-		.option('-c, --config [value]', 'Path to config file used to test. Defaults to ./test/smoke.js')
-		.description('Tests that a given set of urls for an app respond as expected. Expects the config file ./test/smoke.js to exist')
+		.option('-c, --config [value]', 'Path to config file used to test. Defaults to ./test/smoke.json')
+		.description('Tests that a given set of urls for an app respond as expected. Expects the config file ./test/smoke.json to exist')
 		.action(smokeTests.run);
 };
