bf7e38e77360370e1973f460a54d83b453148a23
Arjun Gadhia <arjungadhia@gmail.com>
2018-01-15 12:53:23 +0000

    Authentication  🐿 v2.6.0

--- tasks/smoke.js
+++ tasks/smoke.js
@@ -5,7 +5,7 @@ const smokeTests = require('../lib/smoke');
 module.exports = (program) => {
 	program
 		.command('smoke')
-		// .option('--auth', 'Authenticate with FT_NEXT_BACKEND_KEY')
+		.option('-a', '--auth', 'Authenticate with FT_NEXT_BACKEND_KEY')
 		.option('-h, --host [value]', 'Set the hostname to use for all tests')
 		.option('-c, --config [value]', 'Path to config file used to test. Defaults to ./test/smoke.json')
 		.description('Tests that a given set of urls for an app respond as expected. Expects the config file ./test/smoke.json to exist')
