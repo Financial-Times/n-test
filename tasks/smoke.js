0d8370be02fccf1dc6df3a56853d02874d1c6397
Arjun Gadhia <arjungadhia@gmail.com>
2018-01-22 17:40:13 +0000

    Refactor and fix some stuff  🐿 v2.6.0

--- tasks/smoke.js
+++ tasks/smoke.js
@@ -7,5 +7,14 @@ module.exports = (program) => {
 		.option('-h, --host [value]', 'Set the hostname to use for all tests')
 		.option('-c, --config [value]', 'Path to config file used to test. Defaults to ./test/smoke.json')
 		.description('Tests that a given set of urls for an app respond as expected. Expects the config file ./test/smoke.json to exist')
-		.action(smokeTests.run);
+		.action((opts) => {
+			smokeTests.run(opts).catch(err => {
+				if(err.failed) {
+					console.error(`${err.failed.length} URLs failed their check.`);
+				} else {
+					console.error(err);
+				}
+				process.exit(0);
+			})
+		});
 };
