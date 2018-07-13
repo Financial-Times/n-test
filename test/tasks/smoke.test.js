275f38e50b302f8b24694d0c5a7e1dfc96c9b123
Simon Plenderleith <simon@simonplend.co.uk>
2018-07-13 13:00:46 +0100

    Add tests for validation of config headers
     🐿 v2.8.0

--- test/tasks/smoke.test.js
+++ test/tasks/smoke.test.js
@@ -2,6 +2,7 @@
 
 const server = require('../server/app');
 const SmokeTest = require('../../lib/smoke/smoke-test');
+const { NTestConfigError } = require('../../lib/errors');
 const { spawn } = require('child_process');
 
 describe('Smoke Tests of the Smoke', () => {
@@ -11,7 +12,8 @@ describe('Smoke Tests of the Smoke', () => {
 		server.listen(3004);
 	});
 
-	describe('status checks', () => {
+	describe('Status checks', () => {
+
 		test('tests should pass if all the urls return the correct status', (done) => {
 			const smoke = new SmokeTest({
 				host: 'http://localhost:3004',
@@ -38,9 +40,10 @@ describe('Smoke Tests of the Smoke', () => {
 				done();
 			});
 		}, 10000);
+
 	});
 
-	describe('tests that error', () => {
+	describe('Tests that error', () => {
 
 		test('should handle non-assertion errors gracefully beyond a threshold', (done) => {
 			const smoke = new SmokeTest({
@@ -72,10 +75,10 @@ describe('Smoke Tests of the Smoke', () => {
 
 		});
 
-
 	});
 
 	describe('Adding custom checks', () => {
+
 		test('should allow adding custom assertions',(done) => {
 
 			const smoke = new SmokeTest({
@@ -99,10 +102,12 @@ describe('Smoke Tests of the Smoke', () => {
 				done();
 			});
 		});
+
 	});
 
 	//TODO: figure out how to test the www.ft.com bit!
 	describe('Session tokens', () => {
+
 		test('should not run user-based tests on localhost', () => {
 
 			const smoke = new SmokeTest({
@@ -119,6 +124,7 @@ describe('Smoke Tests of the Smoke', () => {
 	});
 
 	describe('CLI task', () => {
+
 		test('should exit with the correct code for a passing test', (done) => {
 			const proc = spawn('./bin/n-test.js', ['smoke', '--host', 'http://localhost:3004', '--config', 'test/fixtures/smoke-pass.js']);
 			proc.on('close', (code) => {
@@ -134,5 +140,41 @@ describe('Smoke Tests of the Smoke', () => {
 				done();
 			});
 		}, 10000);
+
 	});
+
+	describe('Config validation', () => {
+
+		test('tests should run and pass when all headers in a config are valid', (done) => {
+
+			const smoke = new SmokeTest({
+				host: 'http://localhost:3004',
+				config: 'test/fixtures/smoke-valid-headers.js'
+			});
+
+			return smoke.run().then((results) => {
+				expect(results.passed.length).toEqual(1);
+				expect(results.failed.length).toEqual(0);
+				done();
+			});
+
+		}, 10000);
+
+		test('should throw an NTestConfigError when config contains an invalid header', async () => {
+
+			const smoke = new SmokeTest({
+				host: 'http://localhost:3004',
+				config: 'test/fixtures/smoke-invalid-header.js'
+			});
+
+			await expect(smoke.run()).rejects.toThrowError(NTestConfigError);
+
+		}, 10000);
+
+		// TODO: Add test for a null header once functionality to pass config
+		// object has been added i.e. config for these validation tests can be
+		// multiple exports in one config file
+
+	});
+
 });
